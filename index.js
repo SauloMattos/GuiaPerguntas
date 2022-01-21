a6e3436a7121f1ac861060c70fc9769585f0b006
Saulo Mattos <saulomm15@gmail.com>
2022-01-21 17:51:14 -0300

    Aula 37 ok

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //Dizendo para o Express para usar o EJS como view engine
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Rotas
 app.get("/",(req, res) =>{
     res.render("index");   
 });
@@ -17,6 +23,4 @@ app.post("/salvarpergunta",(req, res) => {
     res.send("Formulário Recebido!");
 });
 
-app.listen(8080, () => {
-    console.log("App Rodando!");
-});
+app.listen(8080, () => {console.log("App Rodando!");});
